0ca9aa16c77dbd8958d884da91ea1a886837ba79
Jason Fulbright <jason2348@hotmail.com>
2019-07-31 20:18:26 -0400

    7/31/19 updates

---
+++ recentpost.js
@@ -0,0 +1,47 @@
+document.write(" <section class='fashion-area section-gap' id='recentpost'>\
+  <div class='container'>\
+    <div class='row'>\
+      <div class='col-lg-3 col-md-6 single-fashion'>\
+        <img class='img-fluid' src='img/f1.jpg' alt=''>\
+        <p class='date'>10 Jan 2018</p>\
+        <h4><a href=''#''>Addiction When Gambling  Becomes A Problem</a></h4>\
+        <p>  inappropriate behavior ipsum dolor sit amet, consectetur.  </p>\
+        <div class='meta-bottom d-flex justify-content-between'>\
+          <p><span class='lnr lnr-heart'></span> 15 Likes</p>\
+          <p><span class='lnr lnr-bubble'></span> 02 Comments</p>\
+        </div>\
+        </div>\
+        <div class='col-lg-3 col-md-6 single-fashion'>\
+          <img class='img-fluid' src='img/f1.jpg' alt=''>\
+          <p class='date'>10 Jan 2018</p>\
+          <h4><a href=''#''>Addiction When Gambling  Becomes A Problem</a></h4>\
+          <p>  inappropriate behavior ipsum dolor sit amet, consectetur.  </p>\
+          <div class='meta-bottom d-flex justify-content-between'>\
+            <p><span class='lnr lnr-heart'></span> 15 Likes</p>\
+            <p><span class='lnr lnr-bubble'></span> 02 Comments</p>\
+          </div>\
+          </div>\
+          <div class='col-lg-3 col-md-6 single-fashion'>\
+            <img class='img-fluid' src='img/f1.jpg' alt=''>\
+            <p class='date'>10 Jan 2018</p>\
+            <h4><a href=''#''>Addiction When Gambling  Becomes A Problem</a></h4>\
+            <p>  inappropriate behavior ipsum dolor sit amet, consectetur.  </p>\
+            <div class='meta-bottom d-flex justify-content-between'>\
+              <p><span class='lnr lnr-heart'></span> 15 Likes</p>\
+              <p><span class='lnr lnr-bubble'></span> 02 Comments</p>\
+            </div>\
+            </div>\
+            <div class='col-lg-3 col-md-6 single-fashion'>\
+              <img class='img-fluid' src='img/f1.jpg' alt=''>\
+              <p class='date'>10 Jan 2018</p>\
+              <h4><a href=''#''>Addiction When Gambling  Becomes A Problem</a></h4>\
+              <p>  inappropriate behavior ipsum dolor sit amet, consectetur.  </p>\
+              <div class='meta-bottom d-flex justify-content-between'>\
+                <p><span class='lnr lnr-heart'></span> 15 Likes</p>\
+                <p><span class='lnr lnr-bubble'></span> 02 Comments</p>\
+              </div>\
+              </div>\
+        </div>\
+        </div>\
+        </section>\
+        ");
