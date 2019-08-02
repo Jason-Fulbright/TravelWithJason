4495251b497db5c38c71af05a45d0ec901fa6713
Jason Fulbright <jason2348@hotmail.com>
2019-08-02 00:25:26 -0400

    big updates baby

--- recentpost.js
+++ recentpost.js
@@ -1,11 +1,12 @@
 document.write(" <section class='fashion-area section-gap' id='recentpost'>\
   <div class='container'>\
+  <h3 class='text-uppercase'>Recent Post</h3>\
     <div class='row'>\
       <div class='col-lg-3 col-md-6 single-fashion'>\
-        <img class='img-fluid' src='img/f1.jpg' alt=''>\
-        <p class='date'>10 Jan 2018</p>\
-        <h4><a href=''#''>Addiction When Gambling  Becomes A Problem</a></h4>\
-        <p>  inappropriate behavior ipsum dolor sit amet, consectetur.  </p>\
+        <img class='img-fluid' src='img/island2.jpg' height='200'>\
+        <p class='date'>1 Aug 2019</p>\
+        <h4><a href='galap.html'></a>Bienvenidos a La Galapagos</h4>\
+        <p> The islanders of the Galapagos are some of the friendliest people you can meet... </p>\
         <div class='meta-bottom d-flex justify-content-between'>\
           <p><span class='lnr lnr-heart'></span> 15 Likes</p>\
           <p><span class='lnr lnr-bubble'></span> 02 Comments</p>\
