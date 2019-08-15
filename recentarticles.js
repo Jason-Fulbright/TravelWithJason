2344e79602c1b500de54d2f8eb875ac30d2e3830
Jason Fulbright <jason2348@hotmail.com>
2019-08-15 01:01:04 -0400

    updates

--- recentarticles.js
+++ recentarticles.js
@@ -1,7 +1,7 @@
 document.write(" <div class='active-recent-carusel'>\
     <div class='item'>\
         <img src='img/asset/slider.jpg' alt=''>\
-        <a href='fluoridefree.html'><p class='mt-20 title text-uppercase'>Finding the Right Toothpaste</p></a>\
+        <a href='fluoridefree.html'><p class='mt-20 title text-uppercase'> Why Choose Fluoride-Free?</p></a>\
         <p>1 Sept, 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 06 <i class='fa fa-comment-o' aria-hidden='true'></i>02</span></p>\
     </div>\
     <div class='item'>\
--- recentdestinations.js
+++ recentdestinations.js
@@ -1,17 +1,17 @@
 document.write(" <div class='active-recent-carusel'>\
     <div class='item'>\
     <img class='img-fluid' src='img/island2.JPG' height='200'>\
-        <p class='mt-20 title text-uppercase'>Bienvenidos a La Galapagos</p>\
+        <a href='galap.html'><p class='mt-20 title text-uppercase'>Hola From the Galapagos</p></a>\
         <p>1 Aug 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 06 <i class='fa fa-comment-o' aria-hidden='true'></i>02</span></p>\
     </div>\
     <div class='item'>\
     <img class='img-fluid' src='pics/costarica/IMG2848.JPG' height='200'  alt=''>\
-        <p class='mt-20 title text-uppercase'>The Magic of Costa Rica</p>\
+        <a href='costarica.html'><p class='mt-20 title text-uppercase'>The Magic of Costa Rica</p></a>\
         <p>6 Aug 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 06 <i class='fa fa-comment-o' aria-hidden='true'></i>02</span></p>\
     </div>\
     <div class='item'>\
     <img class='img-fluid' src='pics/bocas/IMG4320.JPG' height='200' alt=''>\
-        <p class='mt-20 title text-uppercase'>Beautiful Bocas</p>\
+      <a href='bocas.html'>  <p class='mt-20 title text-uppercase'>Beautiful Bocas</p></a>\
         <p>6 Aug 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 06 <i class='fa fa-comment-o' aria-hidden='true'></i>02</span></p>\
     </div>\
 </div>\
