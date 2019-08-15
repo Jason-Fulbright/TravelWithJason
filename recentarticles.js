244a114cf758726419efa8e814f5362e9a343638
Jason Fulbright <jason2348@hotmail.com>
2019-08-14 20:32:33 -0400

    update

--- recentarticles.js
+++ recentarticles.js
@@ -1,13 +1,13 @@
 document.write(" <div class='active-recent-carusel'>\
     <div class='item'>\
         <img src='img/asset/slider.jpg' alt=''>\
-        <p class='mt-20 title text-uppercase'>Home Audio Recording <br>For Everyone</p>\
-        <p>02 Hours ago <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 06 <i class='fa fa-comment-o' aria-hidden='true'></i>02</span></p>\
+        <a href='fluoridefree.html'><p class='mt-20 title text-uppercase'>Finding the Right Toothpaste</p></a>\
+        <p>1 Sept, 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 06 <i class='fa fa-comment-o' aria-hidden='true'></i>02</span></p>\
     </div>\
     <div class='item'>\
         <img src='img/asset/slider.jpg' alt=''>\
-        <p class='mt-20 title text-uppercase'>Home Audio Recording <br>For Everyone</p>\
-        <p>02 Hours ago <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 06 <i class='fa fa-comment-o' aria-hidden='true'></i>02</span></p>\
+        <a href='reuseablebags.html'><p class='mt-20 title text-uppercase'>Investing in Bags</p></a>\
+        <p>8 Sept, 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 06 <i class='fa fa-comment-o' aria-hidden='true'></i>02</span></p>\
     </div>\
     <div class='item'>\
         <img src='img/asset/slider.jpg' alt=''>\
