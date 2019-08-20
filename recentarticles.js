ad3379c1f55c65ccc2e0b6d881ee563306ea3b83
Jason Fulbright <jason2348@hotmail.com>
2019-08-19 23:55:21 -0500

    updates

--- recentarticles.js
+++ recentarticles.js
@@ -2,17 +2,17 @@ document.write(" <div class='active-recent-carusel'>\
     <div class='item'>\
         <img src='img/asset/slider.jpg' alt=''>\
         <a href='fluoridefree.html'><p class='mt-20 title text-uppercase'> Why Choose Fluoride-Free?</p></a>\
-        <p>1 Sept, 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 06 <i class='fa fa-comment-o' aria-hidden='true'></i>02</span></p>\
+        <p>1 Sept 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 0 <i class='fa fa-comment-o' aria-hidden='true'></i>0</span></p>\
     </div>\
     <div class='item'>\
         <img src='img/asset/slider.jpg' alt=''>\
         <a href='reuseablebags.html'><p class='mt-20 title text-uppercase'>Investing in Bags</p></a>\
-        <p>8 Sept, 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 06 <i class='fa fa-comment-o' aria-hidden='true'></i>02</span></p>\
+        <p>8 Sept 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 0 <i class='fa fa-comment-o' aria-hidden='true'></i>0</span></p>\
     </div>\
     <div class='item'>\
         <img src='img/asset/slider.jpg' alt=''>\
         <p class='mt-20 title text-uppercase'>Home Audio Recording <br>For Everyone</p>\
-        <p>02 Hours ago <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 06 <i class='fa fa-comment-o' aria-hidden='true'></i>02</span></p>\
+        <p>02 Hours ago <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 0 <i class='fa fa-comment-o' aria-hidden='true'></i>0</span></p>\
     </div>\
 </div>\
 ");
--- recentdestinations.js
+++ recentdestinations.js
@@ -2,17 +2,17 @@ document.write(" <div class='active-recent-carusel'>\
     <div class='item'>\
     <img class='img-fluid' src='img/island2.JPG' height='200'>\
         <a href='galap.html'><p class='mt-20 title text-uppercase'>Hola From the Galapagos</p></a>\
-        <p>1 Aug 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 06 <i class='fa fa-comment-o' aria-hidden='true'></i>02</span></p>\
+        <p>1 Aug 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 0 <i class='fa fa-comment-o' aria-hidden='true'></i>0</span></p>\
     </div>\
     <div class='item'>\
     <img class='img-fluid' src='pics/costarica/IMG2848.JPG' height='200'  alt=''>\
         <a href='costarica.html'><p class='mt-20 title text-uppercase'>The Magic of Costa Rica</p></a>\
-        <p>6 Aug 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 06 <i class='fa fa-comment-o' aria-hidden='true'></i>02</span></p>\
+        <p>6 Aug 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 0 <i class='fa fa-comment-o' aria-hidden='true'></i>0</span></p>\
     </div>\
     <div class='item'>\
     <img class='img-fluid' src='pics/bocas/IMG4320.JPG' height='200' alt=''>\
       <a href='bocas.html'>  <p class='mt-20 title text-uppercase'>Beautiful Bocas</p></a>\
-        <p>6 Aug 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 06 <i class='fa fa-comment-o' aria-hidden='true'></i>02</span></p>\
+        <p>6 Aug 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 0 <i class='fa fa-comment-o' aria-hidden='true'></i>0</span></p>\
     </div>\
 </div>\
 ");
