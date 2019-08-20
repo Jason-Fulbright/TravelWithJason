d7d1ae9bf566713776702cfb821adbd675b36bb3
Jason Fulbright <jason2348@hotmail.com>
2019-08-19 23:47:41 -0500

    no likes

--- recentpost.js
+++ recentpost.js
@@ -7,40 +7,28 @@ document.write(" <section class='fashion-area section-gap' id='recentpost'>\
         <p class='date'>1 Aug 2019</p>\
         <h4> <a href='galap.html'>Bienvenidos a La Galapagos</a></h4>\
         <p> The islanders of the Galapagos are some of the friendliest people you can meet... </p>\
-        <div class='meta-bottom d-flex justify-content-between'>\
-          <p><span class='lnr lnr-heart'></span> 15 Likes</p>\
-          <p><span class='lnr lnr-bubble'></span> 02 Comments</p>\
-        </div>\
+      
         </div>\
         <div class='col-lg-3 col-md-6 single-fashion'>\
           <img class='img-fluid' src='pics/costarica/IMG2848.JPG' height='200'  alt=''>\
           <p class='date'>1 Aug 2019</p>\
           <h4><a href='costarica.html'>The Magic of Costa Rica</a> </h4>\
           <p>There was always good food around in Costa Rica. The traditional Costa Rican meal consisted of a grilled meat or fish, rice, beans...</p>\
-          <div class='meta-bottom d-flex justify-content-between'>\
-            <p><span class='lnr lnr-heart'></span> 15 Likes</p>\
-            <p><span class='lnr lnr-bubble'></span> 02 Comments</p>\
-          </div>\
+
           </div>\
           <div class='col-lg-3 col-md-6 single-fashion'>\
             <img class='img-fluid' src='pics/bocas/IMG4320.JPG' height='200' alt=''>\
             <p class='date'>1 Aug 2019</p>\
             <h4><a href='bocas.html'>Beautiful Bocas </a> </h4>\
             <p> Getting around these islands was very fun. One of the main methods of transportation is water taxi which I LOVE...</p>\
-            <div class='meta-bottom d-flex justify-content-between'>\
-              <p><span class='lnr lnr-heart'></span> 15 Likes</p>\
-              <p><span class='lnr lnr-bubble'></span> 02 Comments</p>\
-            </div>\
+
             </div>\
             <div class='col-lg-3 col-md-6 single-fashion'>\
               <img class='img-fluid' src='img/f1.jpg' alt=''>\
               <p class='date'>1 Sept 2019</p>\
               <h4><a href='fluoridefree.html'>Choosing the Right Toothpaste</a></h4>\
               <p>   When buying a new toothpaste there are soo many options, do I get a whitening toothpaste, does it need... </p>\
-              <div class='meta-bottom d-flex justify-content-between'>\
-                <p><span class='lnr lnr-heart'></span> 15 Likes</p>\
-                <p><span class='lnr lnr-bubble'></span> 02 Comments</p>\
-              </div>\
+
               </div>\
         </div>\
         </div>\
