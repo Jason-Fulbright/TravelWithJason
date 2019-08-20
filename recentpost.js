224523e019b81d6ed3de641f970f7479153f219c
Jason Fulbright <jason2348@hotmail.com>
2019-08-19 23:49:07 -0500

    Update recentpost.js

--- recentpost.js
+++ recentpost.js
@@ -7,28 +7,24 @@ document.write(" <section class='fashion-area section-gap' id='recentpost'>\
         <p class='date'>1 Aug 2019</p>\
         <h4> <a href='galap.html'>Bienvenidos a La Galapagos</a></h4>\
         <p> The islanders of the Galapagos are some of the friendliest people you can meet... </p>\
-      
         </div>\
         <div class='col-lg-3 col-md-6 single-fashion'>\
           <img class='img-fluid' src='pics/costarica/IMG2848.JPG' height='200'  alt=''>\
           <p class='date'>1 Aug 2019</p>\
           <h4><a href='costarica.html'>The Magic of Costa Rica</a> </h4>\
           <p>There was always good food around in Costa Rica. The traditional Costa Rican meal consisted of a grilled meat or fish, rice, beans...</p>\
-
           </div>\
           <div class='col-lg-3 col-md-6 single-fashion'>\
             <img class='img-fluid' src='pics/bocas/IMG4320.JPG' height='200' alt=''>\
             <p class='date'>1 Aug 2019</p>\
             <h4><a href='bocas.html'>Beautiful Bocas </a> </h4>\
             <p> Getting around these islands was very fun. One of the main methods of transportation is water taxi which I LOVE...</p>\
-
             </div>\
             <div class='col-lg-3 col-md-6 single-fashion'>\
               <img class='img-fluid' src='img/f1.jpg' alt=''>\
               <p class='date'>1 Sept 2019</p>\
               <h4><a href='fluoridefree.html'>Choosing the Right Toothpaste</a></h4>\
               <p>   When buying a new toothpaste there are soo many options, do I get a whitening toothpaste, does it need... </p>\
-
               </div>\
         </div>\
         </div>\
