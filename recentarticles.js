b76224f5e43f2bab6b9cfaa32872ae6b4be4da81
Jason Fulbright <jason2348@hotmail.com>
2019-08-27 11:28:10 -0500

    aug

--- recentarticles.js
+++ recentarticles.js
@@ -2,22 +2,22 @@ document.write(" <div class='active-recent-carusel'>\
     <div class='item'>\
         <img src='pics/costa-5.JPG' alt=''>\
         <a href='fluoridefree.html'><p class='mt-20 title text-uppercase'> Why Choose Fluoride-Free?</p></a>\
-        <p>1 Sept 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 0 <i class='fa fa-comment-o' aria-hidden='true'></i>0</span></p>\
+        <p>1 Aug 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 0 <i class='fa fa-comment-o' aria-hidden='true'></i>0</span></p>\
     </div>\
     <div class='item'>\
         <img src='pics/costa-9.JPG' alt=''>\
         <a href='reuseablebags.html'><p class='mt-20 title text-uppercase'>How to Reduce Your Plastic Footprint Without Breaking the Bank</p></a>\
-        <p>8 Sept 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 0 <i class='fa fa-comment-o' aria-hidden='true'></i>0</span></p>\
+        <p>8 Aug 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 0 <i class='fa fa-comment-o' aria-hidden='true'></i>0</span></p>\
     </div>\
     <div class='item'>\
         <img src='pics/costa-2.JPG' alt=''>\
         <a href='travelcheap.html'><p class='mt-20 title text-uppercase'>How to Travel Europe (And Everywhere Else) CHEAP</p></a>\
-        <p>8 Sept 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 0 <i class='fa fa-comment-o' aria-hidden='true'></i>0</span></p>\
+        <p>19 Aug 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 0 <i class='fa fa-comment-o' aria-hidden='true'></i>0</span></p>\
     </div>\
     <div class='item'>\
-        <img src='img/asset/slider.jpg' alt=''>\
-        <a href='firsthostel.html'><p class='mt-20 title text-uppercase'>My First Hostel</p></a>\
-        <p>8 Sept 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 0 <i class='fa fa-comment-o' aria-hidden='true'></i>0</span></p>\
+        <img src='pics/costa-13.JPG' alt=''>\
+        <a href='fit.html'><p class='mt-20 title text-uppercase'>Staying Healthy 101</p></a>\
+        <p>27 Aug 2019 <span> <i class='fa fa-heart-o' aria-hidden='true'></i> 0 <i class='fa fa-comment-o' aria-hidden='true'></i>0</span></p>\
     </div>\
 </div>\
 ");
--- recentpost.js
+++ recentpost.js
@@ -5,24 +5,24 @@ document.write(" <section class='fashion-area section-gap' id='recentpost'>\
       <div class='col-lg-3 col-md-6 single-fashion'>\
         <img class='img-fluid' src='img/island2.JPG' height='200'>\
         <p class='date'>1 Aug 2019</p>\
-        <h4> <a href='galap.html'>Bienvenidos a La Galapagos</a></h4>\
+        <h4> <a href='galap.html'>Hola from the Galapagos</a></h4>\
         <p>The islanders of the Galapagos are some of the friendliest people you can meet... </p>\
         </div>\
         <div class='col-lg-3 col-md-6 single-fashion'>\
-          <img class='img-fluid' src='pics/costa-5.JPG' height='200'>\
-          <p class='date'>1 Sept 2019</p>\
-          <h4><a href='fluoridefree.html'>Choosing the Right Toothpaste</a></h4>\
-          <p>When buying a new toothpaste there are soo many options, do I get a whitening toothpaste, does it need... </p>\
+          <img class='img-fluid' src='pics/costa-13.JPG' height='200'>\
+          <p class='date'>27 Aug 2019</p>\
+          <h4><a href='fit.html'>Staying Healthy 101</a></h4>\
+          <p>Let's start with what you eat, or actually how much you eat. Full meals are more important than you think.... </p>\
           </div>\
           <div class='col-lg-3 col-md-6 single-fashion'>\
             <img class='img-fluid' src='pics/bocas/IMG4320.JPG' height='200' alt=''>\
-            <p class='date'>1 Aug 2019</p>\
+            <p class='date'>10 Aug 2019</p>\
             <h4><a href='bocas.html'>Beautiful Bocas </a> </h4>\
             <p> Getting around these islands was very fun. One of the main methods of transportation is water taxi which I LOVE...</p>\
             </div>\
             <div class='col-lg-3 col-md-6 single-fashion'>\
               <img class='img-fluid' src='pics/costa-9.JPG'   alt=''>\
-              <p class='date'>1 Aug 2019</p>\
+              <p class='date'>8 Aug 2019</p>\
               <h4><a href='reuseablebags.html'>How to Reduce Your Plastic Footprint Without Breaking the Bank</a> </h4>\
               <p>Grocery bags carry very little economic value so this is not a financial investment but rather a...</p>\
               </div>\
